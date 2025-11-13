4396b1c6127a14a66ecf4fcfeb721f60708d2888
Tnoob-dev <cristiandeleonmonzon@gmail.com>
2025-11-12 20:19:04 -0500

    fixes in translation

--- bot/queries/queries.py
+++ bot/queries/queries.py
@@ -9,10 +9,11 @@ from pathlib import Path
 import os
 
 
-
 @bot.on_callback_query()
 async def query_manager(client: Client, query: CallbackQuery):
 
+    user_id = query.from_user.id
+    
     if query.data == 'order_ready':
         if check_administration(query):
                 await client.send_message(chat_id="chat1080p", 
@@ -22,20 +23,29 @@ async def query_manager(client: Client, query: CallbackQuery):
         else:
             await query.answer(text="🤨", show_alert=True)
     else:
+        main_path = Path.cwd() / Path("bot") / Path("translations")
         target_lang = query.data.split("_")[1]
-        input_file = Path.cwd() / Path("bot") / Path("translations") / Path("downloads") / Path(str(query.from_user.id)) / Path(return_filename().split("/")[-1])
         
-        output_file = Path.cwd() / Path("bot") / Path("translations") / Path("output") / Path(str(query.from_user.id)) / Path(f"{target_lang.upper()}_{return_filename().split("/")[-1]}")
+        input_str =  main_path / Path("downloads") / Path(str(user_id)) / Path(return_filename().split("/")[-1])
+        output_str = main_path / Path("output") / Path(str(user_id)) / Path(f"{target_lang.upper()}_{return_filename().split("/")[-1]}")
         
         translator = Translate()
-        await translator.srt_translate(target_lang, input_file, output_file)
+        
         await query.message.delete()
         
-        m = await query.message.reply(f"Traduciendo {return_filename().split("/")[-1]}, espere por favor, en cuanto termine tendra su subtitulo subido, mientras tanto, disfrute de nuestro canal 😉")
+        m = await query.message.reply(f"👨‍🔧Traduciendo ||__{return_filename().split("/")[-1]}__||, espere por favor, en cuanto termine tendra su subtitulo subido, mientras tanto, disfrute de nuestro canal 😉")
         
-        await query.message.reply_document(output_file, caption=f"Archivo traducido para el usuario {query.from_user.mention} ({query.from_user.id})")
+        await translator.srt_translate(target_lang, str(input_str), str(output_str))
+        await query.message.reply_document(output_str, caption=f"✅Archivo traducido✅\nPara el usuario {query.from_user.mention} ({query.from_user.id})")
         
         await m.delete()
         
-        os.remove(input_file)
-        os.remove(output_file)
+        download_files = os.listdir(main_path / Path("downloads") / Path(str(user_id)))
+        output_files = os.listdir(main_path / Path("output") / Path(str(user_id)))
+        
+        for dl,ot in zip(download_files, output_files):
+            os.remove(main_path / Path("downloads") / Path(str(user_id)) / dl)
+            os.remove(main_path / Path("output") / Path(str(user_id)) / ot)
+        
+        os.rmdir(main_path / Path("downloads") / Path(str(user_id)))
+        os.rmdir(main_path / Path("output") / Path(str(user_id)))
