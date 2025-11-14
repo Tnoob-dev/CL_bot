e404ee8c6b914718196c9e58f8eceb7036445a0b
Tnoob-dev <cristiandeleonmonzon@gmail.com>
2025-11-14 11:47:01 -0500

    ...

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,6 +3,7 @@ from commands.Hello import hello
 from commands.Collection import start_collection, end_collection, collect_messages
 from commands.Order import get_orders
 from commands.Translations import translate_srt
+from commands.Watch import watch
 from queries.queries import query_manager
 from utils.create_paths import create_translations_path, create_download_path
 from utils.db_reqs import start_daily_reset
@@ -26,6 +27,7 @@ bot.add_handler(MessageHandler(collect_messages))
 bot.add_handler(MessageHandler(end_collection))
 bot.add_handler(MessageHandler(get_orders))
 bot.add_handler(MessageHandler(translate_srt))
+bot.add_handler(MessageHandler(watch))
 
 # Queries
 bot.add_handler(CallbackQueryHandler(query_manager))
---
+++ bot/commands/Watch.py
@@ -0,0 +1,8 @@
+from entry.entry import bot
+from pyrogram.client import Client
+from pyrogram.types import Message
+from pyrogram.filters import command, private
+
+@bot.on_message(command("h") & private)
+async def watch(client: Client, message: Message):
+    await message.reply("XDDDDDDDD")
