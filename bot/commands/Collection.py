f30dbc1ced798d70f8d85b8800625d631f505d12
Tnoob-dev <cristiandeleonmonzon@gmail.com>
2025-11-21 22:23:28 -0500

    added massive command, new things coming ;)

--- bot/commands/Collection.py
+++ bot/commands/Collection.py
@@ -1,17 +1,45 @@
 from entry.entry import bot
-from utils.functions import check_administration
+from utils.functions import check_administration, forward_messages
 from utils.db_reqs import insert
 from db.create_cine_db import Game
 from pyrogram.client import Client
 from pyrogram.types import Message
-from pyrogram.filters import command, private, document, video, photo, user
-from pyrogram.errors import FloodWait
+from pyrogram.filters import command, private, document, video, photo
 from pathlib import Path
-import asyncio
+from typing import List
 import os
 
-# state for saving messages of files
+# state for saving messages of files and other data
 state = {}
+
+# massive command, to add a lot of files, in only one command, instead of multiple /add commands
+@bot.on_message(command("massive") & private)
+async def massive_collection(client: Client, message: Message):
+    user_id = message.from_user.id
+    
+    if check_administration(message):
+        if not str(user_id) in state:
+            state[str(user_id)] = {
+                "collecting": True,
+                "massive_mode": True,
+                "messages": [],
+                "links": []
+            }
+            
+            await message.reply("Modo masivo activado, envie /end para determinar por temporadas, y envie /end_massive para terminar el modo masivo")
+
+# end_massive command, to end the complete task
+@bot.on_message(command("end_massive") & private)
+async def end_massive(client: Client, message: Message):
+    user_id = message.from_user.id
+    links = state[str(user_id)]["links"] # get links from the user state dict
+    unlinked_urls = '\n'.join(links) # put one down another
+    
+    await message.reply(f"Aqui tiene todos los enlaces:\n{unlinked_urls}")
+    await message.reply_document(document=Path.cwd() / Path("bot") / Path("core") / "cine.db")
+    
+    # delete user from memory
+    del state[str(user_id)] 
 
 # add command, for start adding messages id to state var
 @bot.on_message(command("add") & private)
@@ -24,6 +52,7 @@ async def start_collection(client: Client, message: Message):
         if not str(user_id) in state: # if the user is not in state, add it with all the followings schemas
             state[str(user_id)] = {
                 "collecting": True,
+                "massive_mode": False,
                 "messages": []
             }
             
@@ -35,43 +64,45 @@ async def start_collection(client: Client, message: Message):
 # end command, finish command add, and forward all the messages to the backup channel
 @bot.on_message(command("end") & private)
 async def end_collection(client: Client, message: Message):
-    
+    user_id = message.from_user.id
     try:
         if check_administration(message): # check if user is admin
              
-            if not str(message.from_user.id) in state: # if the user id is not in state, means that the user never used the command add
+            if not str(user_id) in state: # if the user id is not in state, means that the user never used the command add
                 await message.reply("Usted no se encuentra en el modo coleccion")
             else:
-                messages = state[str(message.from_user.id)]["messages"] # get all the messages that the user collected
-                
-                # for loop into messaages to send all the file to backup channel
-                for message_id in messages:
-                    success = False # Flag: if True, means file sent to backup channel succesfully
+                if state[str(user_id)]["massive_mode"]:
+                    messages: List[int] = state[str(user_id)]["messages"]
                     
-                    # while loop...
-                    while not success:
-                        try:
-                            
-                            # forwarding
-                            await client.copy_message(chat_id=os.getenv("CHANNEL_ID"), from_chat_id=os.getenv("SENDER_BOT"), message_id=message_id)
-                            success = True # change flag to True and go for the next file
-                            
-                        except FloodWait as f: # if exists flood sleep bot the time estimated
-                            
-                            await asyncio.sleep(f.value)
-                            
-                if len(state[str(message.from_user.id)]["messages"]) > 0:
-                    await message.reply("Mensaje(s) enviados al chat de backup")
-                
-                # Once all the files has been forwarded, insert code to DB
-                insert(Game(name="new_" + str(messages[-1]), file_ids=messages))
-                
-                
-                await message.reply(f"Ha salido del modo coleccion, para iniciar una nueva coleccion escriba el comando /add\n\nSu enlace es https://t.me/{os.getenv("SENDER_BOT")}?start=new_{messages[-1]}")
-                await message.reply_document(document=Path.cwd() / Path("bot") / Path("core") / "cine.db")
-                
-                # delete user from memory
-                del state[str(message.from_user.id)]
+                    await forward_messages(client, messages) # forward messages to backup channel
+                    
+                    # Once all the files has been forwarded, insert code to DB
+                    insert(Game(name="new_" + str(messages[-1]), file_ids=messages))
+                    
+                    # append links of files, to the "links" schema in the user dict
+                    state[str(user_id)]["links"].append(f"https://t.me/{os.getenv("SENDER_BOT")}?start=new_{messages[-1]}")
+                    
+                    # clear list for receive new messages
+                    messages.clear()
+                    
+                    await message.reply("Mensajes reenviados, envie la siguiente temporada, o envie /end_massive")
+                else:
+                    messages = state[str(user_id)]["messages"] # get all the messages that the user collected
+                    
+                    await forward_messages(client, messages) # forward messages to backup channel
+                                
+                    if len(state[str(user_id)]["messages"]) > 0:
+                        await message.reply("Mensaje(s) enviados al chat de backup")
+                    
+                    # Once all the files has been forwarded, insert code to DB
+                    insert(Game(name="new_" + str(messages[-1]), file_ids=messages))
+                    
+                    
+                    await message.reply(f"Ha salido del modo coleccion, para iniciar una nueva coleccion escriba el comando /add\n\nSu enlace es https://t.me/{os.getenv("SENDER_BOT")}?start=new_{messages[-1]}")
+                    await message.reply_document(document=Path.cwd() / Path("bot") / Path("core") / "cine.db")
+                    
+                    # delete user from memory
+                    del state[str(user_id)]
     except IndexError:
         # if is not a document, video or photo, isn't a valid file
         await message.reply("Ha enviado un tipo de archivo no valido, intentelo de nuevo")
@@ -81,8 +112,8 @@ async def end_collection(client: Client, message: Message):
 # this will only wait for a document, video or photo
 @bot.on_message(private & document | video | photo)
 async def collect_messages(client: Client, message: Message):
-    
+    user_id = message.from_user.id
     # check user is admin
     if check_administration(message):
-        if str(message.from_user.id) in state: # if user id is in state, means its in collection mode
-            state[str(message.from_user.id)]["messages"].append(message.id) # just append to that schema
+        if str(user_id) in state: # if user id is in state, means its in collection mode
+            state[str(user_id)]["messages"].append(message.id) # just append to that schema
--- bot/utils/functions.py
+++ bot/utils/functions.py
@@ -1,9 +1,10 @@
 from pyrogram.client import Client
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.errors import UserNotParticipant
+from pyrogram.errors import UserNotParticipant, FloodWait
 from typing import List
 from pathlib import Path
 import os
+import asyncio
 
 # check if a user is admin
 def check_administration(message: Message) -> bool:
@@ -40,3 +41,20 @@ async def check_user_in_channel(client: Client, message: Message) -> bool:
     except Exception as e:
         print(f"Error inesperado en check_user_in_channel: {e}")
         return False
+
+async def forward_messages(client: Client, messages: List[int]):
+    # for loop into messaages to send all the file to backup channel
+    for message_id in messages:
+        success = False # Flag: if True, means file sent to backup channel succesfully
+        
+        # while loop...
+        while not success:
+            try:
+                
+                # forwarding
+                await client.copy_message(chat_id=os.getenv("CHANNEL_ID"), from_chat_id=os.getenv("SENDER_BOT"), message_id=message_id)
+                success = True # change flag to True and go for the next file
+                
+            except FloodWait as f: # if exists flood sleep bot the time estimated
+                
+                await asyncio.sleep(f.value)
