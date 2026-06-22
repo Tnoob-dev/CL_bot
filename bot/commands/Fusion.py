4efa804399405915649c6cf3e560fba3a61a1c37
Tnoob-dev <cristiandeleonmonzon@gmail.com>
2026-06-22 19:46:55 -0400

    everythings working, adding last fixes

--- bot/commands/Fusion.py
+++ bot/commands/Fusion.py
@@ -12,32 +12,38 @@ async def fusion_posts(client: Client, message: Message):
 
     if check_administration(message) and message.command is not None:
 
-        if message.command is not None and len(message.command) == 3:
+        if len(message.command) == 3:
             
             command = message.command
 
-            post1_id = int(command[-2])
-            post2_id = int(command[-1])
-
-            if post1_id == None or post2_id == None:
-                await message.reply("Uno o ambos ids no son validos")
+            try:
+                post1_id = int(command[-2])
+                post2_id = int(command[-1])
+            except ValueError:
+                await message.reply("Los IDs deben ser numeros validos")
                 return
             
+            chat_id = os.getenv("CINEMA_ID")
+
             message_info = await client.get_messages(
-                chat_id=os.getenv("CINEMA_ID"),
+                chat_id=chat_id,
                 message_ids=[post1_id, post2_id]
             )
 
             post1_info = message_info[0]
             post2_info = message_info[1]
 
+            combined_keyboard = []
+            combined_keyboard.extend(post1_info.reply_markup.inline_keyboard)
+            combined_keyboard.extend(post2_info.reply_markup.inline_keyboard)
 
-            post1_info.reply_markup.inline_keyboard.extend(post2_info.reply_markup.inline_keyboard)
+            try:
+                await client.edit_message_reply_markup(
+                    chat_id=chat_id,
+                    message_id=post1_id,
+                    reply_markup=InlineKeyboardMarkup(combined_keyboard)
+                )
 
-            await client.edit_message_reply_markup(
-                chat_id=os.getenv("CINEMA_ID"),
-                message_id=post1_id,
-                reply_markup=InlineKeyboardMarkup(post1_info.reply_markup.inline_keyboard)
-            )
-
-            await message.reply("Post editado con el nuevo inline markup")
+                await message.reply("Post editado con el nuevo inline markup✅📝")
+            except Exception as e:
+                await message.reply(e)
