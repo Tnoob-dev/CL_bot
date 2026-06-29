91bd6d6410217732c74af647b1e786fbf14a996e
Cristian Manuel de León Monzón <cristiandeleonmonzon@gmail.com>
2026-06-29 19:03:46 -0400

    Update functions.py

--- bot/utils/functions.py
+++ bot/utils/functions.py
@@ -40,7 +40,7 @@ async def check_user_in_channel(client: Client, message: Message) -> bool:
     
     try:
         await client.get_chat_member(chat_id=os.getenv("CINEMA_ID"), user_id=message.from_user.id)
-        # await client.get_chat_member(chat_id=os.getenv("GUEST_ID"), user_id=message.from_user.id)
+        await client.get_chat_member(chat_id=os.getenv("GUEST_ID"), user_id=message.from_user.id)
 
         return True
     except UserNotParticipant:
@@ -48,7 +48,8 @@ async def check_user_in_channel(client: Client, message: Message) -> bool:
         await message.reply("Para usar este bot, primero debes unirte a nuestros canales.", 
                             reply_markup=InlineKeyboardMarkup(
                                 [
-                                    [InlineKeyboardButton("🎬Cinema Library🎬", url=f"https://t.me/{os.getenv("CINEMA_ID")}")]
+                                    [InlineKeyboardButton("🎬Cinema Library🎬", url=f"https://t.me/{os.getenv("CINEMA_ID")}")],
+                                    [InlineKeyboardButton(os.getenv("GUEST_NAME"), url=os.getenv("GUEST_LINK"))]
                                 ]
                             ))
         return False
