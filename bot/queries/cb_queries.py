ac184475971b888448b0e50762907f4d63d5c553
Tnoob-dev <cristiandeleonmonzon@gmail.com>
2026-07-16 13:37:30 -0400

    fixes in logic of stream.py

--- bot/queries/cb_queries.py
+++ bot/queries/cb_queries.py
@@ -202,10 +202,10 @@ async def query_manager(client: Client, query: CallbackQuery):
         
         text = f"""
 Coste del plan VIP 💎: 
-    - 💳 Tarjeta {os.getenv('VIP_PRICE_CUP')} CUP
-    - 💳 Tarjeta {os.getenv('VIP_PRICE_MLC')} MLC
-    - 💵 USD/PayPal/Zelle {os.getenv('VIP_PRICE_USD')} USD
-    - 📱 Saldo Movil{os.getenv('VIP_PRICE_CUP')} CUP
+    - 💳 Tarjeta ➡️ {os.getenv('VIP_PRICE_CUP')} CUP
+    - 💳 Tarjeta ➡️ {os.getenv('VIP_PRICE_MLC')} MLC
+    - 💵 USD/PayPal/Zelle ➡️ {os.getenv('VIP_PRICE_USD')} USD
+    - 📱 Saldo Movil ➡️ {os.getenv('VIP_PRICE_CUP')} CUP
 
 Ventajas que ofrece el plan 📈:
     - Stream de archivos y videos 📺
