3e16e5a3131a9cbd0bf6dbe2eea2598630595ee0
Tnoob-dev <cristiandeleonmonzon@gmail.com>
2026-08-18 22:08:36 -0400

    added tmdb info search

--- bot/utils/movie_search.py
+++ bot/utils/movie_search.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import aiohttp
 import os
 import logging
@@ -7,10 +9,14 @@ from aiohttp.http_exceptions import HttpBadRequest
 
 logger = logging.getLogger(__name__)
 
-async def get_results(query: str):
+##### IMDB
+
+async def get_results(query: str) -> list | None:
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"{os.getenv("IMDB_API_URL")}/search/titles?query={query}&limit=5") as response:
+        async with (
+            aiohttp.ClientSession() as session,
+            session.get(f"{os.getenv("IMDB_API_URL")}/search/titles?query={query}&limit=5") as response
+        ):
                 titles = await response.json()
 
                 if not titles.get("titles"):
@@ -29,11 +35,13 @@ async def get_results(query: str):
         return results
     except (ClientError, ClientConnectionError, ConnectionTimeoutError, HttpBadRequest) as error:
         logger.error(error)
-
-async def get_info_by_id(movieId: str):
+        
+async def get_info_by_id(movieId: str) -> None | Any:
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"{os.getenv("IMDB_API_URL")}/titles/{movieId}") as response:
+        async with (
+            aiohttp.ClientSession() as session,
+            session.get(f"{os.getenv("IMDB_API_URL")}/titles/{movieId}") as response
+        ):
                 movie = await response.json()
 
                 if not movie.get("id"):
@@ -41,5 +49,67 @@ async def get_info_by_id(movieId: str):
 
                 return movie
 
+    except (ClientError, ClientConnectionError, ConnectionTimeoutError, HttpBadRequest) as error:
+        logger.error(error)
+
+
+##### IMDB
+
+
+async def search_tmdb(query: str):
+    
+    try:
+        headers = {
+            "accept": "application/json",
+            "Authorization": f"Bearer {os.getenv("TMDB_API_KEY")}"
+        }
+        
+        async with (
+            aiohttp.ClientSession() as session,
+            session.get(f"{os.getenv("TMDB_API_URL")}/search/multi?query={query}&include_adult=false&language=en-US&page=1", headers=headers) as response
+        ): 
+            
+            results = dict(await response.json()).get("results")
+            
+            return results
+    
+    except (ClientError, ClientConnectionError, ConnectionTimeoutError, HttpBadRequest) as error:
+        logger.error(error)
+        
+async def get_movie_info_by_id_tmdb(movieId: str):
+    try:
+        headers = {
+            "accept": "application/json",
+            "Authorization": f"Bearer {os.getenv("TMDB_API_KEY")}"
+        }
+        
+        async with (
+            aiohttp.ClientSession() as session,
+            session.get(f"{os.getenv("TMDB_API_URL")}/movie/{movieId}?language=en-US", headers=headers) as response
+        ):
+            
+            result = dict(await response.json())
+            
+            return result
+            
+    except (ClientError, ClientConnectionError, ConnectionTimeoutError, HttpBadRequest) as error:
+        logger.error(error)
+
+async def get_tv_info_by_id_tmdb(seriesId: str):
+    try:
+        headers = {
+            "accept": "application/json",
+            "Authorization": f"Bearer {os.getenv("TMDB_API_KEY")}"
+        }
+        
+        async with (
+            aiohttp.ClientSession() as session,
+            session.get(f"{os.getenv("TMDB_API_URL")}/tv/{seriesId}?language=en-US", headers=headers) as response
+        ):
+            
+            result = dict(await response.json())
+            
+            return result
+            
     except (ClientError, ClientConnectionError, ConnectionTimeoutError, HttpBadRequest) as error:
         logger.error(error)
